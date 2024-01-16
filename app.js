7bc926c1e2fbbc77dc235e98505620f40c0ba865
rajivsuting <rajivsuting16@gmail.com>
2024-01-16 18:28:52 +0530

    push

--- app.js
+++ app.js
@@ -26,21 +26,7 @@ const cors = require("cors");
 
 const port = process.env.PORT || 3000;
 
-const allowedOrigins = ["https://admin.dpccollege.com"];
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-  })
-);
-
-// Handle preflight requests
-app.options("*", cors());
+app.use(cors());
 
 app.use(express.json());
 
