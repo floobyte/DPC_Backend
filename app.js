8fc8b8becd53ec247c81d43800ec8ac1699b103a
rajivsuting <rajivsuting16@gmail.com>
2024-02-05 00:41:29 +0530

    update

--- app.js
+++ app.js
@@ -4,19 +4,8 @@ const app = express();
 
 const cors = require("cors");
 app.use(cors({ origin: "*" }));
-
+const path = require("path");
 require("dotenv/config");
-const port = process.env.PORT || 3000;
-
-const fileUpload = require("express-fileupload");
-app.use(express.json());
-
-app.use(
-  fileUpload({
-    useTempFiles: true,
-  })
-);
-
 const connectDB = require("./db/connectDB");
 const eventRouter = require("./routes/eventRouter");
 const postRouter = require("./routes/postRouter");
@@ -37,26 +26,44 @@ const blogRouter = require("./routes/blogRouter");
 const mentorRouter = require("./routes/mentorRouter");
 const studentRouter = require("./routes/studentRouter");
 
-app.use("/api/event", eventRouter);
-app.use("/api/post", postRouter);
-app.use("/api/course", courseRouter);
-app.use("/api/notification", notificationRouter);
-app.use("/api/user/auth", authRouter);
-app.use("/api/admin", adminRouter, userRouter);
-app.use("/api/staff", staffRouter);
-app.use("/api/notice", noticeRouter);
-app.use("/api/studentcorner", studentCornerRouter);
-app.use("/api/downloaddata", downloadRouter);
-app.use("/api/admission", admissionRouter);
-app.use("/api/department", departmentRouter);
-app.use("/api/blog", blogRouter);
-app.use("/api/student", studentRouter);
+const port = process.env.PORT;
+
+const fileUpload = require("express-fileupload");
+app.use(express.json());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
+
+app.use("/event", eventRouter);
+app.use("/post", postRouter);
+app.use("/course", courseRouter);
+app.use("/notification", notificationRouter);
+app.use("/user/auth", authRouter);
+app.use("/admin", adminRouter, userRouter);
+app.use("/staff", staffRouter);
+app.use("/notice", noticeRouter);
+app.use("/studentcorner", studentCornerRouter);
+app.use("/downloaddata", downloadRouter);
+app.use("/admission", admissionRouter);
+app.use("/department", departmentRouter);
+app.use("/blog", blogRouter);
+app.use("/student", studentRouter);
 app.use("/b-u/story", storyRouter);
 app.use("/b-u/mentor", mentorRouter);
 
 app.use(errorHandler);
 
+connectDB();
+
+// app.use(express.static(path.join(__dirname, "./client/build")));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"), (err) => {
+//     res.status(500).send(err);
+//   });
+// });
 app.listen(port, () => {
-  connectDB();
   console.log(`connection is Live at port no. ${port}`);
 });
